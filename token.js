ac5ab2cd25f27360036c8cb9d36e913265f4d8e8
Robinson Ureña Rodríguez <robinsonurodriguez@gmail.com>
2025-08-03 23:43:05 -0400

    FEAT: Add Token class for lexical analysis.

---
+++ token.js
@@ -0,0 +1,12 @@
+class Token {
+  /**
+   * @param {string} type
+   * @param {string} value
+   */
+  constructor(type, value) {
+    this.type = type;
+    this.value = value;
+  }
+}
+
+export { Token };
